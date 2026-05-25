15c6603bb8be2819c4ea3ae4bfde202725258eaf
Sammy Arreaza <lsarreaza@uncg.edu>
2026-05-25 17:14:03 -0400

    Added Springboot to repo

---
+++ sporttracker/src/main/java/com/example/sporttracker/SporttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sporttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SporttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SporttrackerApplication.class, args);
+	}
+
+}
---
+++ sporttracker/src/test/java/com/example/sporttracker/SporttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sporttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SporttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
